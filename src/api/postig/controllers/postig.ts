2d406d8bd5d857c123dad22833cecbd783412f6e
VikasVicky17 <vikas.r.17@gmail.com>
2026-02-23 13:53:50 +0530

    Strapi Insta backend

---
+++ src/api/postig/controllers/postig.ts
@@ -0,0 +1,7 @@
+/**
+ * postig controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::postig.postig');
---
+++ src/api/postig/services/postig.ts
@@ -0,0 +1,7 @@
+/**
+ * postig service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::postig.postig');
